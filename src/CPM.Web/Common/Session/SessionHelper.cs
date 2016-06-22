66bf4931b488b79f8c31c93dc3a795f37f34447b
SEIFU <SEIFU@SEIFU-TOLOSA>
2016-06-22 16:45:38 +0100

    Addded SessionHelper class

---
+++ src/CPM.Web/Common/Session/SessionHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using CpmLib.Web.Core.Session;
+
+namespace CPM.Web.Common.Session
+{
+    public interface ISessionHelper :ISessionConfigBase
+    {
+
+    }
+
+    public class SessionHelper :SessionConfigBase, ISessionHelper
+    {
+        public SessionHelper(IHttpContextAccessor httpContextAccessor) :base(httpContextAccessor)
+        {
+            
+        }  
+        
+        public SessionCPMUserVM CPMUser
+        {
+            get { return Session.GetObjectFromJson<SessionCPMUserVM>("User"); }
+            set { Session.SetObjectAsJson("User", value); }
+        }  
+
+        public void ClearSession()
+        {
+            Session.Clear();
+        }
+    }
+}
