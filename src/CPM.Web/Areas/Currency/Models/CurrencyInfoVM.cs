200227bdb573a2726771f5400d3d2a7930b328b0
seifu <seifu@SEIFU-TOLOSA>
2016-07-06 16:48:56 +0100

    Adjusted Change,Price views on Currency Index

--- src/CPM.Web/Areas/Currency/Models/CurrencyInfoVM.cs
+++ src/CPM.Web/Areas/Currency/Models/CurrencyInfoVM.cs
@@ -21,15 +21,19 @@ namespace CPM.Web.Areas.Currency.Models
 
        // [DisplayFormat(DataFormatString = "{0:#.####}")]
         public decimal Price { get; set; }
-
         
-        public decimal Volume { get; set; }
 
+        public decimal Volume { get; set; }
       
-        public decimal Change { get; set; }       
+        public decimal Change { get; set; }        
 
         public DateTime DateTime { get; set; } 
         public bool Success { get; set; }
         public string Error { get; set; }
+
+        public PriceTickerInfoVM()
+        {
+
+        }
     }
 }
--- src/CPM.Web/wwwroot/js/layout.js
+++ src/CPM.Web/wwwroot/js/layout.js
@@ -1,13 +1,30 @@
-﻿$(function() {
-    $('#nav-menu').kendoMenu();
-    //$.ajax({
-    //    url: "Api/Currency",
-    //    dataType: "json",
-    //    type: "GET",
-    //    success: function (currency) {
-    //        currency.foreach(function (currency){
-    //            var top10 = "<li>< a href='/currency/Index/" + currency.Id + "'>" + currency.Name + "<a></li>";
-    //        });
-    //    }
-    //});
-});
+﻿//$(function() {
+//    $('#nav-menu').kendoMenu();
+//    //$.ajax({
+//    //    url: "Api/Currency",
+//    //    dataType: "json",
+//    //    type: "GET",
+//    //    success: function (currency) {
+//    //        currency.foreach(function (currency){
+//    //            var top10 = "<li>< a href='/currency/Index/" + currency.Id + "'>" + currency.Name + "<a></li>";
+//    //        });
+//    //    }
+//    //});
+//});
+
+//$(function() {    
+
+//    var elem = $('div #tickerChange').text()
+
+//    $('div #tickerdiv').each(function (index) {
+//        var num = $(this).text;
+//        console.log(index + ": " + $(this));
+//    });
+    
+//    //if (elem.data() > 0) {
+//    //    elem.addClass("smta-changeup");      
+//    //}
+//    //else {
+//    //  elem.addClass("smta-changedown");
+//    //}
+//});
